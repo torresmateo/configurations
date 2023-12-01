ca9dcb52c5fdc162d2ac18f1d36fbe9c29a07272
Mateo Torres <torresmateo@gmail.com>
2023-12-01 11:27:25 -0500

    add Cornell aliases

---
+++ omz/aliases.zsh
@@ -0,0 +1,7 @@
+#Aliases
+
+alias conn_webserver='ssh mft53@rs-wi-hy299-r9host.biohpc.cornell.edu'
+alias yl_conn_webserver='ssh yulab@rs-wi-hy299-r9host.biohpc.cornell.edu'
+alias conn_slurm='ssh mft53@cbsulogin.biohpc.cornell.edu'
+alias yl_conn_slurm='ssh yulab@cbsulogin.biohpc.cornell.edu'
+alias conn_server1='ssh mft53@cbsuhy01.biohpc.cornell.edu'
